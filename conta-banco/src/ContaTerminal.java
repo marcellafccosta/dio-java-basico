b2d16967da90bf234ea27e43d40f5068078d27cf
Marcella Chaves <marcellafccosta@gmail.com>
2024-05-28 18:50:04 -0300

    projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da agencia!");
+        int numAgencia = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o número da conta!");
+        String numConta = sc.nextLine();
+
+        System.out.println("Por favor, digite seu nome!");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o valor do saldo para saque!");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia é " + numAgencia + ", conta " + numConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        sc.close();
     }
 }
